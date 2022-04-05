89bf7a29d2c2007d7b8c5afdaa31b378b9ccd296
James <marcosveloso2019@gmail.com>
2022-04-05 12:35:06 -0300

    finish modal

--- src/app/list-calender/components/modal/modal.component.ts
+++ src/app/list-calender/components/modal/modal.component.ts
@@ -1,5 +1,5 @@
 import { CalenderService } from "./../../../services/calender/calender.service";
-import { ModalController } from "@ionic/angular";
+import { ModalController, ToastController } from "@ionic/angular";
 import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
@@ -20,7 +20,8 @@ export class ModalComponent implements OnInit {
 
   constructor(
     private service: CalenderService,
-    private modalCtr: ModalController
+    private modalCtr: ModalController,
+    public toastController: ToastController
   ) {}
 
   ngOnInit() {
@@ -34,40 +35,54 @@ export class ModalComponent implements OnInit {
   }
 
   createUpdateCalendar() {
-    if (this.typeView == "edit") {
-      let dateFormat;
-      let splitDate = this.date.split("-");
-      dateFormat = splitDate[2] + "/" + splitDate[1];
+    try {
+      if (this.typeView == "edit") {
+        let dateFormat;
+        let splitDate = this.date.split("-");
+        dateFormat = splitDate[2] + "/" + splitDate[1];
 
-      this.service
-        .update(this.eventId, {
-          name: this.name,
-          description: this.description,
-          type: this.type,
-          format: this.format,
-          date: dateFormat,
-          year: splitDate[0],
-          hours: this.hour,
-        })
-        .subscribe((e) => console.log(e));
-    } else {
-      let dateFormat;
-      let splitDate = this.date.split("-");
-      dateFormat = splitDate[2] + "/" + splitDate[1];
+        this.service
+          .update(this.eventId, {
+            name: this.name,
+            description: this.description,
+            type: this.type,
+            format: this.format,
+            date: dateFormat,
+            year: splitDate[0],
+            hours: this.hour,
+          })
+          .subscribe((e) => console.log(e));
+      } else {
+        let dateFormat;
+        let splitDate = this.date.split("-");
+        dateFormat = splitDate[2] + "/" + splitDate[1];
 
-      this.service
-        .creteEvent({
-          name: this.name,
-          description: this.description,
-          type: this.type,
-          format: this.format,
-          date: dateFormat,
-          year: splitDate[0],
-          hours: this.hour,
-        })
-        .subscribe((e) => console.log(e));
+        this.service
+          .creteEvent({
+            name: this.name,
+            description: this.description,
+            type: this.type,
+            format: this.format,
+            date: dateFormat,
+            year: splitDate[0],
+            hours: this.hour,
+          })
+          .subscribe((e) => console.log(e));
+          this.modalCtr.dismiss();
+
+      }
+    } catch {
+      this.presentToast("Algum campo obrigatorio está preenchido errado")
     }
-    this.modalCtr.dismiss();
+  }
+
+  async presentToast(msg) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 1000,
+      color : "danger"
+    });
+    toast.present();
   }
 
   disabledInput() {
